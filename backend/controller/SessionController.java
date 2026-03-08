9e86701ebe90cc373b47c0d0392f5d0d63c15ebb
Christopher Ridad <christopherridad@gmail.com>
2026-03-07 18:32:55 -0600

    pull main into feature/session-endpoints, keep Ade's entity classes

--- backend/controller/SessionController.java
+++ backend/controller/SessionController.java
@@ -5,6 +5,11 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import com.billsplit.backend.model.Session;
+import com.billsplit.backend.model.CreateSessionResult;
+import com.billsplit.backend.model.JoinSessionResult;
+import com.billsplit.backend.model.Member;
+
 @Controller
 public class SessionController {
 
